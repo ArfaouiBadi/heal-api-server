57a5dd725acb56ea1e1fbafb50b8ce3e006071ef
ArfaouiBadi <arfaouibadi19@gmail.com>
2024-01-23 08:21:19 +0100

    finished working on product CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,21 +1,66 @@
-/*
-https://docs.nestjs.com/controllers#controllers
-*/
-
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from '@prisma/client';
+
+interface CreateProductDto {
+  ProductName: string;
+  Marque: string;
+  price: number;
+  Quantity: number;
+  ExpirationDate: Date;
+  ProductCategory: string;
+  inventoryStatus: { value: string }; // Assuming it's an object with a "value" property
+  Image: string;
+}
 
 @Controller('products')
 export class ProductController {
   constructor(private productService: ProductService) {}
-  @Get('')
+
+  @Get()
   getProducts(): Promise<Product[]> {
     return this.productService.getProducts();
   }
+
   @Post('addProduct')
-  async addProduct(@Body() req: any): Promise<Product> {
-    console.log(req);
-    return this.productService.addProduct(req);
+  async addProduct(@Body() req: CreateProductDto): Promise<Product> {
+    try {
+      const product = await this.productService.addProduct(req);
+      return product;
+    } catch (error) {
+      console.error('Error adding product:', error);
+      throw new Error('Error adding product');
+    }
+  }
+
+  @Put()
+  async updateProduct(@Body() req): Promise<Product> {
+    try {
+      const updatedProduct = await this.productService.updateProduct(req);
+      return updatedProduct;
+    } catch (error) {
+      console.error('Error updating product:', error);
+      throw new Error('Error updating product');
+    }
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: string): Promise<Product> {
+    console.log(id);
+    try {
+      const deletedProduct = await this.productService.deleteProduct(id);
+      return deletedProduct;
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      throw new Error('Error deleting product');
+    }
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,21 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+interface CreateProductDto {
+  ProductName: string;
+  Marque: string;
+  price: number;
+  Quantity: number;
+  ExpirationDate: Date;
+  ProductCategory: string;
+  inventoryStatus: { value: string }; // Assuming it's an object with a "value" property
+  Image: string;
+}
+
+interface UpdateProductDto extends CreateProductDto {
+  id: string;
+}
 
 @Injectable()
 export class ProductService {
@@ -9,6 +24,7 @@ export class ProductService {
   async getProducts(): Promise<Product[]> {
     return this.prisma.product.findMany();
   }
+
   async getProductsByCategory(category: string): Promise<Product[]> {
     return this.prisma.product.findMany({
       where: {
@@ -16,6 +32,7 @@ export class ProductService {
       },
     });
   }
+
   async getProductsByStatus(status: string): Promise<Product[]> {
     return this.prisma.product.findMany({
       where: {
@@ -23,6 +40,7 @@ export class ProductService {
       },
     });
   }
+
   async getProductsByUser(userId: string): Promise<Product[]> {
     return this.prisma.product.findMany({
       where: {
@@ -31,34 +49,71 @@ export class ProductService {
     });
   }
 
-  async addProduct(@Body() req: any): Promise<Product> {
-    console.log(req);
-    const product = await this.prisma.product.create({
-      data: {
-        ProductName: req.ProductName,
-        Marque: req.Marque,
-        Price: req.price,
-        Quantity: req.Quantity,
-        ExpirationDate: req.ExpirationDate,
-        ProductCategory: req.ProductCategory,
-        Status: req.inventoryStatus.value,
-        Image: req.Image,
-        Reviews: 0,
-        user: {
-          connect: {
-            id: '65ac63178d7f7bd033f1d6f3',
+  async addProduct(req: CreateProductDto): Promise<Product> {
+    try {
+      const product = await this.prisma.product.create({
+        data: {
+          ProductName: req.ProductName,
+          Marque: req.Marque,
+          Price: req.price,
+          Quantity: req.Quantity,
+          ExpirationDate: req.ExpirationDate,
+          ProductCategory: req.ProductCategory,
+          Status: req.inventoryStatus.value,
+          Image: req.Image,
+          Reviews: 0,
+          user: {
+            connect: {
+              id: '65ac63178d7f7bd033f1d6f3',
+            },
           },
         },
-      },
-    });
-    return product;
+      });
+      return product;
+    } catch (error) {
+      console.error('Error creating product:', error);
+      throw new Error('Error creating product');
+    }
   }
-  updateProduct(): string {
-    return 'update product';
+
+  async updateProduct(req: UpdateProductDto): Promise<Product> {
+    try {
+      const updatedProduct = await this.prisma.product.update({
+        where: {
+          id: req.id,
+        },
+        data: {
+          ProductName: req.ProductName,
+          Marque: req.Marque,
+          Price: req.price,
+          Quantity: req.Quantity,
+          ExpirationDate: req.ExpirationDate,
+          ProductCategory: req.ProductCategory,
+          Status: req.inventoryStatus.value,
+          Image: req.Image,
+        },
+      });
+      return updatedProduct;
+    } catch (error) {
+      console.error('Error updating product:', error);
+      throw new Error('Error updating product');
+    }
   }
-  deleteProduct(): string {
-    return 'delete product';
+
+  async deleteProduct(id: string): Promise<Product> {
+    try {
+      const deletedProduct = await this.prisma.product.delete({
+        where: {
+          id: id,
+        },
+      });
+      return deletedProduct;
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      throw new NotFoundException('Product not found');
+    }
   }
+
   getProduct(): string {
     return 'get product';
   }
