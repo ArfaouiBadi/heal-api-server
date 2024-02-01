f9f35df21afd54f71bf7b99ef0b209f94a219821
ArfaouiBadi <arfaouibadi19@gmail.com>
2024-02-01 05:35:35 +0100

    added CRUD for user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Controller('User')
 export class UserController {
   constructor(private userService: UserService) {}
-  @Get('hello')
-  getUser(): string {
-    return 'Hello World!';
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<User> {
+    return this.userService.getUserById(id);
   }
-  @Post()
-  postUser() {
-    return this.userService.postUser();
+  @Put(':id')
+  async updateUser(@Body() req, @Param('id') id: string): Promise<User> {
+    console.log(req);
+    return this.userService.updateUser(id, req);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,13 +4,39 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
-  async postUser() {
-    const User = await this.prisma.user.create({
-      data: {
-        email: 'Arfaoui Badii',
-        password: '123456789',
-      },
-    });
-    return User;
+
+  async getUserById(id: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id: id },
+      });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      console.error('Error in getUserById:', error.message);
+      throw new Error('Failed to get user by ID');
+    }
+  }
+  async updateUser(id: string, data: any) {
+    try {
+      console.log(id, data);
+
+      const user = await this.prisma.user.update({
+        where: { id: id },
+        data: {
+          email: data.email,
+          phone: data.phone,
+          address: data.address,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.error('Error in updateUser:', error.message);
+      throw new Error('Failed to update user');
+    }
   }
 }
